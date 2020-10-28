d751e80bdd5873510b041115ced4f12a3d0ff59e
Blake Cooper <accounts@blake-cooper.com>
2020-10-28 16:47:09 -0400

    Made second backup optional

--- mcserverbackup.sh
+++ mcserverbackup.sh
@@ -5,5 +5,9 @@
 if [ $(python3 getActivePlayers.py $serverAddress) == "0" ] && [ $(stat -c %Y ${pathToServer}/logs/latest.log) -gt $(stat -c %Y $pathToBackup1) ]
 then
 	zip -r $pathToBackup1 $pathToServer
-	cp $pathToBackup1 $pathToBackup2 
+
+	if [ $pathToBackup ne '']
+	then
+		cp $pathToBackup1 $pathToBackup2 
+	fi
 fi
