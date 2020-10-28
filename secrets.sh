e08f7acf831fa9eaaf85dd29b169970e84d1b10f
Blake Cooper <contact@blake-cooper.com>
2020-10-28 16:45:24 -0400

    Update secrets.sh

--- secrets.sh
+++ secrets.sh
@@ -1,4 +1,4 @@
-serverAddress='72.75.255.236:25565'
-pathToServer='server'
-pathToBackup1='backups/minecraftserver_backup.zip'
-pathToBackup2='/media/blake-and-dave/MCS_backup'
+serverAddress=''
+pathToServer=''
+pathToBackup1=''
+pathToBackup2=''
