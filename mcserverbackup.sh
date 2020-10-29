38910c4703bdcb84443addf63e288fc7c8ff5064
Blake Cooper <accounts@blake-cooper.com>
2020-10-29 13:36:01 -0400

    Added multiple backups

--- mcserverbackup.sh
+++ mcserverbackup.sh
@@ -2,14 +2,52 @@
 
 source secrets.sh
 
-if [ $(python3 getActivePlayers.py $serverAddress) == "0" ] && [ $(stat -c %Y server/logs/latest.log) -gt $(stat -c %Y $pathToBackup1) ]
-then 
-	echo "Success!"
-	zip -r $pathToBackup1 $pathToServer
+if [ $(python3 getActivePlayers.py $serverAddress) == "0" ] && [ $(stat -c %Y $pathToServer/logs/latest.log) -gt $(stat -c %Y $pathToBackup1/minecraftserver_backup_1.zip) ]
+then
 
-	if [ $pathToBackup -ne '']
+	i=$[$numberOfBackups]
+
+	echo "backup needed..."	
+	if [[ -f $pathToBackup1/minecraftserver_backup_$numberOfBackups.zip ]]
 	then
-		cp $pathToBackup1 $pathToBackup2 
+		echo "maximum number of backups reached"
+		rm $pathToBackup1/minecraftserver_backup_$numberOfBackups.zip
+		if [[ $pathToBackup2 != "" ]]
+		then
+			rm $pathToBackup2/minecraftserver_backup_$numberOfBackups.zip
+		fi
+		i=$[$i-1]
+	fi
+
+	$echo "Max number of backups: $i"
+	while [[ $i -gt 0 ]]
+	do
+		echo "i = $i"
+		if [[ -f $pathToBackup1/minecraftserver_backup_$i.zip ]]
+		then
+			mv $pathToBackup1/minecraftserver_backup_$i.zip $pathToBackup1/minecraftserver_backup_$[$i+1].zip
+		fi	
+		
+		i=$[$i-1]
+	done
+
+	zip -r $pathToBackup1/minecraftserver_backup_1.zip $pathToServer
+
+	if [[ $pathToBackup2 != "" ]]
+	then
+	
+		i=$numberOfBackups
+
+		while [[ $i > 0 ]]
+		do
+			if [[ -f $pathToBackup2/minecraftserver_backup_$i.zip ]]
+			then
+				mv $pathToBackup2/minecraftserver_backup_$i.zip $pathToBackup2/minecraftserver_backup_$[$i+1].zip
+			fi
+			i=$[$i-1] 
+		done
+
+		cp $pathToBackup1/minecraftserver_backup_1.zip $pathToBackup2 
 	fi
 
 fi
