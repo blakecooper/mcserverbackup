d1c97de2bd677224d12112751d22e19711d40f0f
Blake Cooper <accounts@blake-cooper.com>
2020-10-29 08:47:39 -0400

    Fixed logic bug

--- mcserverbackup.sh
+++ mcserverbackup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-./secrets.sh
+source secrets.sh
 
-if [ $(python3 getActivePlayers.py $serverAddress) == "0" ] && [ $(stat -c %Y ${pathToServer}/logs/latest.log) -gt $(stat -c %Y $pathToBackup1) ]
-then
+if [ $(python3 getActivePlayers.py $serverAddress) == "0" ] && [ $(stat -c %Y server/logs/latest.log) -gt $(stat -c %Y $pathToBackup1) ]
+then 
+	echo "Success!"
 	zip -r $pathToBackup1 $pathToServer
 
-	if [ $pathToBackup ne '']
+	if [ $pathToBackup -ne '']
 	then
 		cp $pathToBackup1 $pathToBackup2 
 	fi
+
 fi
